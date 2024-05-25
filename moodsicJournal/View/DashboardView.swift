80ddf8e31a99b7cb30a1e2dcfd7b4155a666e19b
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-05-25 20:57:32 +0700

    Fix: Music played at dashboard bug

--- moodsicJournal/View/DashboardView.swift
+++ moodsicJournal/View/DashboardView.swift
@@ -16,68 +16,80 @@ struct DashboardView: View {
     private var journals: FetchedResults<Journal>
 
     @State private var isAddJournalOpen = false
-
-    @State  var musicAuthorizationStatus: MusicAuthorization.Status
-    @State  var isAuthViewShowed: Bool
-
-    private let player = ApplicationMusicPlayer.shared
-
+    @State var musicAuthorizationStatus: MusicAuthorization.Status
+    @State var isAuthViewShowed: Bool
+    @State private var selectedJournal: Journal?
 
     var body: some View {
-        GeometryReader {
-            geometry in
-            NavigationSplitView(
-                sidebar: {},
-                detail: {
-                    VStack(alignment: .leading) {
-                        Text("Dashboard")
-                        Button(action: {
-                            isAddJournalOpen.toggle()
-                        }, label: {
-                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                                .fill(.white)
-                                .frame(width: 200, height: 200)
-                                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                                .overlay {
-                                    Image(systemName: "plus.circle")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .foregroundStyle(Color.blue)
-                                        .frame(maxWidth: 70)
-                                }
-                        })
-                        .buttonStyle(PlainButtonStyle())
-                        .sheet(isPresented: $isAddJournalOpen, content: {
-                            CustomModalView().environment(\.managedObjectContext, viewContext)
-                        })
-                        .padding(.bottom, 20)
+        GeometryReader { geometry in
+            NavigationStack {
+                VStack(alignment: .leading) {
+                                        Text("Dashboard")
+                                        Button(action: {
+                                            isAddJournalOpen.toggle()
+                                        }, label: {
+                                            RoundedRectangle(cornerRadius: 25.0)
+                                                .fill(.white)
+                                                .frame(width: 200, height: 200)
+                                                .shadow(radius: 10)
+                                                .overlay {
+                                                    Image(systemName: "plus.circle")
+                                                        .resizable()
+                                                        .aspectRatio(contentMode: .fit)
+                                                        .foregroundStyle(Color.blue)
+                                                        .frame(maxWidth: 70)
+                                                }
+                                        })
+                                        .buttonStyle(PlainButtonStyle())
+                                        .sheet(isPresented: $isAddJournalOpen, content: {
+                                            CustomModalView().environment(\.managedObjectContext, viewContext)
+                                        })
+                                        .padding(.bottom, 20)
 
-                        List {
-                            ForEach(journals){journal in
-                                NavigationLink(destination: JournalView(id: journal.id, data: journal.canvasData, title: journal.title, objectId: journal.objectID, mood: journal.mood, SongId: journal.songId, isMoodSelected: true), label: {
-                                    Text(journal.title ?? "Untitled")
-                                })
+                                        VStack {
+                                            ForEach(journals) { journal in
+                                                NavigationLink(destination: JournalView(viewModel: {
+                                                    {
+                                                        let viewModel = JournalViewModel()
+                                                        viewModel.id = journal.id
+                                                        viewModel.data = journal.canvasData
+                                                        viewModel.title = journal.title
+                                                        viewModel.objectId = journal.objectID
+                                                        viewModel.mood = journal.mood
+                                                        viewModel.songId = journal.songId
+                                                        return viewModel
+                                                    }()
+                                                }()), label: {Text(journal.title ?? "Untitled")})
+                                                //                                NavigationLink(
+                                                //                                    destination: JournalView(viewModel: {
+                                                //                                        let viewModel = JournalViewModel()
+                                                //                                        viewModel.id = journal.id
+                                                //                                        viewModel.data = journal.canvasData
+                                                //                                        viewModel.title = journal.title
+                                                //                                        viewModel.objectId = journal.objectID
+                                                //                                        viewModel.mood = journal.mood
+                                                //                                        viewModel.songId = journal.songId
+                                                //                                        return viewModel
+                                                //                                    }()),
+                                                //                                    tag: journal,
+                                                //                                    selection: $selectedJournal
+                                                //                                ) {
+                                                //                                    Text(journal.title ?? "Untitled")
+                                                //                                }
+                                            }
+                                        }
+                                        .sheet(isPresented: $isAuthViewShowed, content: {
+                                            MusicKitAuthorizationView(musicAuthorizationStatus: $musicAuthorizationStatus, isAuthViewShowed: $isAuthViewShowed)
+                                        })
+                                        .padding(.horizontal, geometry.size.width * 0.05)
+                                    }
 
-                            }
-                        }
-                        .onAppear {
-                            Task {
-                                player.stop()
-                                print("song should be stopped")
-                            }
-                    }
-                    .sheet(isPresented: $isAuthViewShowed, content: {
-                        MusicKitAuthorizationView(musicAuthorizationStatus: $musicAuthorizationStatus, isAuthViewShowed: $isAuthViewShowed)
-                    })
-                    .padding(.horizontal, geometry.size.width * 0.05)
-                }
-                })
+            }
         }
-
-
-
     }
 }
+
+
 
 //#Preview {
 //    DashboardView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
--- moodsicJournal/View/JournalView.swift
+++ moodsicJournal/View/JournalView.swift
@@ -13,158 +13,56 @@ struct JournalView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @Environment(\.dismiss) private var dismiss
 
-    @State var id:UUID?
-    @State var data:Data?
-    @State var title:String?
-    @State var objectId:NSManagedObjectID?
-    @State var mood:String?
-    @State var SongId:String?
-
-    @State var isMoodSelected:Bool
-    private let player = ApplicationMusicPlayer.shared
-    @State var songs = [Song]()
-
+    @ObservedObject var viewModel: JournalViewModel
 
     var body: some View {
-        GeometryReader {
-            geometry in
+        GeometryReader { geometry in
             NavigationStack {
                 ZStack {
-                   if isMoodSelected {
-                        VStack(alignment: .leading) {
-                            HStack {
-                                HStack {
-                                    Button(action: {dismiss()}, label: {
-                                        Image(systemName: "chevron.left")
-                                    })
-                                    .buttonStyle(PlainButtonStyle())
-
-                                    Text(title ?? "Untitled")
-                                    Text(mood ?? "No Mood")
-                                    Text(SongId ?? "No Selected Song")
-
-                                    HStack {
-                                    Text("Music")
-                                    }
-                                }
-                                Spacer()
-
-
-                                Menu {
-                                    Button("Delete Journal", action: {deleteJournal()})
-                                } label: {
-                                    Image(systemName: "ellipsis.circle")
-                                }
-
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Button(action: { dismiss() }) {
+                                Image(systemName: "chevron.left")
                             }
-                            .padding(.horizontal, geometry.size.width * 0.05)
-                            .padding(.vertical, geometry.size.height * 0.02)
+                            .buttonStyle(PlainButtonStyle())
 
-                            JournalCanvasView(data: data ?? Data(), id: id ?? UUID())
-                                .environment(\.managedObjectContext, viewContext)
-                                                        //                    .navigationTitle(title ?? "Untitled")
+                            Text(viewModel.title ?? "Untitled")
+                            Text(viewModel.mood ?? "No Mood")
+                            Text(viewModel.songId ?? "No Selected Song")
+
+                            Spacer()
+
+                            Menu {
+                                Button("Delete Journal", action: {
+                                    viewModel.deleteJournal(in: viewContext)
+                                    dismiss()
+                                })
+                            } label: {
+                                Image(systemName: "ellipsis.circle")
+                            }
                         }
+                        .padding(.horizontal, geometry.size.width * 0.05)
+                        .padding(.vertical, geometry.size.height * 0.02)
 
-                   } else {
-//                       Rectangle()
-//                           .frame(width: geometry.size.width, height: geometry.size.height)
-//                           .ignoresSafeArea()
-//                           .opacity(0.5)
-//                           .overlay {
-//                               RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/) 
-//                                   .fill(.white)
-//                                   .overlay
-//                               {
-//                                   VStack {
-//                                       MoodSelectionModalView()
-//                                           .clipShape(.rect(cornerRadius: 25))
-//                                           .padding(.horizontal, 50)
-//                                           .padding(.bottom, 20)
-//                                       Button(action: {isMoodSelected.toggle()}, label: {
-//                                           Text("Next")
-//                                       })
-//                                   }
-//
-//                               }
-//                                .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.35)
-//
-//                           }
-                   }
-
-
+                        JournalCanvasView(data: viewModel.data ?? Data(), id: viewModel.id ?? UUID())
+                            .environment(\.managedObjectContext, viewContext)
+                    }
                 }
                 .onAppear {
                     Task {
-                        await getSongs()
+                        await viewModel.fetchSongs()
                     }
+                }
+                .onDisappear {
+                    viewModel.stopMusic()
                 }
             }
         }
     }
-
-    private func deleteJournal() {
-        guard let objectId = objectId else {
-            print("No object ID available to delete")
-            return
-        }
-
-        do {
-            let object = try viewContext.existingObject(with: objectId)
-            viewContext.delete(object)
-
-            // Save the context to persist the deletion
-            try viewContext.save()
-            dismiss()
-            print("Object deleted successfully")
-        } catch {
-            print("Failed to delete object with ID \(objectId): \(error)")
-        }
-    }
-
-    private func getSongs() async {
-           do {
-               var searchRequest = MusicCatalogResourceRequest<Song>(matching: \.id, equalTo: MusicItemID(rawValue: SongId!))
-//               searchRequest.limit = 3
-               let searchResponse = try await searchRequest.response()
-
-               guard let songPlayed = searchResponse.items.first else { return }
-
-
-               await play(song: songPlayed)
-//               // Map the search results to your SongItem model
-//                          let fetchedSongs = searchResponse.songs.compactMap { song in
-//                              SongItem(id: song.id.rawValue, title: song.title, artist: song.artistName, imageURL: song.artwork?.url(width: 100, height: 100))
-//                          }
-//
-//                          // Update the state on the main thread
-//                          DispatchQueue.main.async {
-//                              self.songs = fetchedSongs
-//                          }
-               print("This is the fetched song by ID: \(searchResponse)")
-           } catch {
-               print("Error fetching songs: \(error)")
-           }
-       }
-
-    func play(song: Song) async {
-      let player = ApplicationMusicPlayer.shared
-      player.queue = [song]
-        do {try await player.prepareToPlay()}
-        catch {
-            print(error)
-        }
-        do {
-            try await player.play()
-            print("should be played")
-
-        }
-        catch {
-            print(error)
-
-        }
-    }
 }
 
-#Preview {
-    JournalView(id: UUID(), data: Data(), isMoodSelected: false)
-}
+
+
+//#Preview {
+//    JournalView(id: UUID(), data: Data())
+//}
